4196ac9c9f5f8c5a72c7467f4852f62632d92431
ursidal <sylvain.vinet@gmail.com>
2018-01-09 16:10:48 +0100

    Update alphabeta.ml

--- alphabeta.ml
+++ alphabeta.ml
@@ -1,3 +1,11 @@
+let iswinner jeu joueur =
+  let liste_coup = depl_possibles jeu joueur in
+  let case_gagnante = match joueur with
+    |Sud -> CaseNord
+    |Nord -> CaseSud
+  in
+    List.exists (fun c -> c = case_gagnante) liste_coup  
+
 let rec alphabeta (jeu,joueur) alpha beta =
   if iswinner jeu joueur then winscore joueur
   else 
